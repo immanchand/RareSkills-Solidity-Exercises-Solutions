3bdf060b1bdce634072b3e0b5be928badfda5390
immanchand <53471578+immanchand@users.noreply.github.com>
2025-02-12 10:49:02 +0400

    solution Add.sol

--- Add/src/Add.sol
+++ Add/src/Add.sol
@@ -1,9 +1,9 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract Add {
-    function add(uint256 a, uint256 b) public pure returns (uint256) {
-        // your code here
-	some chane again;
+contract Add{
+    function add(uint256 x, uint256 y) public pure returns(uint256){
+        uint256 sum = x + y;
+        return sum;
     }
 }
